36e60c2e4fb663381b683d90fb54fea09bcfceb4
Alexander <shiale14@student.westerdals.no>
2017-03-03 23:15:53 +0100

    rearrange project structure and add webpack and webpack-dev-server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alexandershipunov on 02/03/2017.
+ **/
+import React from 'react'
+import { render } from 'react-dom'
+import { hello, goodbye } from './lib'
+
+render(
+    <div>
+        {hello}
+        {goodbye}
+    </div>,
+    document.getElementById('react-container')
+);
