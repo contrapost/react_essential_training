f6009ee681d2917e3508de04524d5e36e7fc42ef
Alexander <shiale14@student.westerdals.no>
2017-03-05 14:17:58 +0100

    refactor component: class -> functional stateless component

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -2,39 +2,30 @@
  * Created by alexandershipunov on 05/03/2017.
  */
 
-import { Component } from 'react';
 import '../stylesheets/ui.scss';
 
-export class SkiDayCount extends Component{
-    percentToDecimal(decimal){
-        return ((decimal * 100) + '%');
-    }
+const percentToDecimal = (decimal) => ((decimal * 100) + '%');
 
-    calcGoalProgress(total, goal){
-        return this.percentToDecimal(total/goal);
-    }
+const calcGoalProgress = (total, goal) => (percentToDecimal(total/goal));
 
-    render() {
-        return (
+export const SkiDayCount = ({ total, powder, backcountry, goal }) =>  (
             <div className="ski-day-count">
                 <div className="total-days">
-                    <span>{this.props.total}</span>
+                    <span>{total}</span>
                     <span>days</span>
                 </div>
                 <div className="powder-days">
-                    <span>{this.props.powder}</span>
+                    <span>{powder}</span>
                     <span>days</span>
                 </div>
                 <div className="backcountry-days">
-                    <span>{this.props.backcountry}</span>
+                    <span>{backcountry}</span>
                     <span>days</span>
                 </div>
                 <div>
                     <span>
-                        {this.calcGoalProgress(this.props.total, this.props.goal)}
+                        {calcGoalProgress(total, goal)}
                         </span>
                 </div>
             </div>
         );
-    }
-}
